0f6a28cbc89299beca433283f83f44254b228ded
Michael Stallings <stall84@gmail.com>
2020-09-15 18:02:47 -0400

    added claasws customer

--- C_Sharp_Classes_1/Customer.cs
+++ C_Sharp_Classes_1/Customer.cs
@@ -6,23 +6,27 @@ namespace C_Sharp_Classes_1
     {
         public int Id;
         public string Name;
-        public List<Order> Order;
+        public List<Order> Orders;
 
 
         public Customer()
         {
-
+            Orders = new List<Order>();         // A List must be initialized by constructor to avoid null ref exception
         }
 
-        public Customer(int id)
+        public Customer(int id)                 // We can ensure that the first parameterless constructor with our Orders initialization always gets
+            : this()                            // called first by adding a : this() syntax to the other constructors
         {
+            
             this.Id = id;
         }
 
         public Customer(int id, string name)
+            : this(id)
         {
-            this.Id = id;
+           
             this.Name = name;
         }
+
     }
 }
--- C_Sharp_Classes_1/Program.cs
+++ C_Sharp_Classes_1/Program.cs
@@ -8,7 +8,14 @@ namespace C_Sharp_Classes_1
         static void Main(string[] args)
         {
 
-            Customer customer = new Customer(1, "John");
+            Customer customer = new Customer();
+
+            customer.Name = "John";
+            customer.Id = 1;
+
+
+            var order = new Order();
+            customer.Orders.Add(order);         // Without providing initialization of list in contsructor, this will throw a Null Ref Exception
 
             Console.WriteLine($"Customer ID: {customer.Id}");
             Console.WriteLine($"Customer Name: {customer.Name}");
