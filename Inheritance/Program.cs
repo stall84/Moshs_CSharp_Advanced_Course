fa9a46e04251e2786b3d01ad5a0b13d8e92c5e18
Michael Stallings <stall84@gmail.com>
2020-09-24 18:06:58 -0400

    Completed Inheritance modeling

---
+++ Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Inheritance
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+
+            text.Width = 100;
+            text.Copy();
+            
+        }
+    }
+}
---
+++ Inheritance/Text.cs
@@ -0,0 +1,16 @@
+﻿namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            System.Console.WriteLine($"A link was added to: {url}");
+
+        }
+
+
+    }
+}
