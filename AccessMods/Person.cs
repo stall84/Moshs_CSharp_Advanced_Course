4c72bdf320e07cc0fd40151f59eb5380eb406729
Michael Stallings <stall84@gmail.com>
2020-09-24 10:59:11 -0400

    Finished Properties section using Auto-properties and constructor private set initialization

--- AccessMods/Person.cs
+++ AccessMods/Person.cs
@@ -1,36 +1,55 @@
 ﻿using System;
 
 namespace AccessMods
+
 {
     class Person
 
         // Evolution of Birthdate get/set methods shown below with furthest down = oldest/most verbose 
     {
-        public DateTime Birthdate { get; set; }     // Takes it even further. Allow the C# compiler to create the private
-                                                    // readonly field for you, also handling the actual getting and setting codeblock logic
-        //private DateTime _birthdate;
+        public DateTime Birthdate { get; private set; }     // Takes it even further. Allow the C# compiler to create the private
+                                                            // The private accessor before the set means the field can ONLY be set at initialization via Constructor
+                                                            // readonly field for you, also handling the actual getting and setting codeblock logic
 
-        //public DateTime Birthdate
-        //{
-        //    get { return _birthdate; }      // Simply returning the private field .. just like the lengthy GetBirthdate method below commented out
-        //    set { _birthdate = value; }     // Using value keyword to get whatever will be on the right side of the assignment at runtime
-        //}
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;                           // Allows setting the birtday only once at object initialization
+        }
 
-        //public string _name;
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
 
-        //public Person(string name)
-        //{
-        //    this._name = name;                                // Converting all of this commented-out block to simplified
-        //}                                                     // Getter/Setter Properties (foregoing the name prop/ctor)
+                return years;
+            }
 
-        //public void SetBirthdate(DateTime birthdate)
-        //{
-        //    this._birthdate = birthdate;
-        //}
+        }
+            //private DateTime _birthdate;
 
-        //public DateTime GetBirthdate()
-        //{
-        //    return _birthdate;
-        //}
-    }
+            //public DateTime Birthdate
+            //{
+            //    get { return _birthdate; }      // Simply returning the private field .. just like the lengthy GetBirthdate method below commented out
+            //    set { _birthdate = value; }     // Using value keyword to get whatever will be on the right side of the assignment at runtime
+            //}
+
+            //public string _name;
+
+            //public Person(string name)
+            //{
+            //    this._name = name;                                // Converting all of this commented-out block to simplified
+            //}                                                     // Getter/Setter Properties (foregoing the name prop/ctor)
+
+            //public void SetBirthdate(DateTime birthdate)
+            //{
+            //    this._birthdate = birthdate;
+            //}
+
+            //public DateTime GetBirthdate()
+            //{
+            //    return _birthdate;
+            //}
+        }
 }
--- AccessMods/Program.cs
+++ AccessMods/Program.cs
@@ -2,36 +2,27 @@
 
 namespace AccessMods
 {
-    class Person
-    {
-        private DateTime _birthdate;
-        public string _name;
-
-        public Person(string name)
-        {
-            this._name = name;
-        }
-
-        public void SetBirthdate(DateTime birthdate)
-        {
-            this._birthdate = birthdate;
-        }
-
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person("Michael");
 
-            person.SetBirthdate(new DateTime(1984, 2, 16));
+            var person = new Person(new DateTime(1984, 02, 16));             // Setting birtdate via constructor
 
-            Console.WriteLine($"Hello {person._name} that was born on {person.GetBirthdate()}");
+            
+
+            Console.WriteLine($"Age of Michael: {person.Age}");
+           
+
+
+            // Below code was used for most-verbose, oldest ways of setting and getting methods from Person. 
+
+            //var person = new Person("Michael");
+
+            //person.SetBirthdate(new DateTime(1984, 2, 16));
+
+            //Console.WriteLine($"Hello {person._name} that was born on {person.GetBirthdate()}");
         }
     }
 }
