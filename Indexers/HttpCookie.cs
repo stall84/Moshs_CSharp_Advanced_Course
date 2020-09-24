0b6cea65427495802ed7a0c6acf37df36740dbca
Michael Stallings <stall84@gmail.com>
2020-09-24 11:14:20 -0400

    added and finished section on Indexer properties

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        // Dictionary creates a hashed-table which makes looking up a value by a key quick and easy. Alternative to List
+
+        private readonly Dictionary<string, string> _dictionary;   // Specifiying the keys should be of type string, Values also type string
+
+
+        public HttpCookie()                             // Initialize dictionary in a constructor
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+            set
+            {
+                _dictionary[key] = value;
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Indexers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cookie = new HttpCookie();
+
+                cookie["name"] = "Michael";
+
+            Console.WriteLine($"Cookie Name: {cookie["name"]}");
+        }
+    }
+}
