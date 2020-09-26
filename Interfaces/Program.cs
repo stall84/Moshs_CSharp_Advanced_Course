9ad5f017163a2285bb608c3f4979721d8cbe56a1
Michael Stallings <stall84@gmail.com>
2020-09-26 15:03:44 -0400

    Explored Interfaces as they relate to Unit Testing

---
+++ Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces
+{
+    // Interface: Similar to classes but fundamentally different. Unlike classes, Interfaces have NO implementation (somewhat similar to abstract modifier earlier)
+    // Interfaces allow for very loosely coupled app-architecture and improve testability of applications.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Interfaces/ShippingCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Interfaces
+{
+    public interface IShippingCalculator
+    {
+        float CalculateShipping(Order order);
+       
+    }
+    public class ShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            if (order.TotalPrice < 30f)
+                return order.TotalPrice * .1f;
+
+            return 0;
+
+
+        }
+    }
+}
